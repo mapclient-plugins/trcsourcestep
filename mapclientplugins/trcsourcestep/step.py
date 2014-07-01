91d56cc57e7a1a0ab905829132c25f854d3c81a7
Hugh Sorby <h.sorby@auckland.ac.nz>
2014-07-01 16:19:00 +1200

    Initial commit of everything.

---
+++ mapclientplugins/trcsourcestep/step.py
@@ -0,0 +1,122 @@
+
+'''
+MAP Client Plugin Step
+'''
+import os
+
+from PySide import QtCore
+
+from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
+from mapclientplugins.trcsourcestep.configuredialog import ConfigureDialog
+from mapclientplugins.trcsourcestep.trcdata import TRCData
+
+
+class TRCSourceStep(WorkflowStepMountPoint):
+    '''
+    Skeleton step which is intended to be a helpful starting point
+    for new steps.
+    '''
+
+    def __init__(self, location):
+        super(TRCSourceStep, self).__init__('TRC Source', location)
+        self._configured = False  # A step cannot be executed until it has been configured.
+        self._category = 'General'
+        # Add any other initialisation code here:
+        # Ports:
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#trcdata'))
+        self._config = {}
+        self._config['identifier'] = ''
+        self._config['Location'] = ''
+
+        self._data = TRCData()
+
+
+    def execute(self):
+        '''
+        Add your code here that will kick off the execution of the step.
+        Make sure you call the _doneExecution() method when finished.  This method
+        may be connected up to a button in a widget for example.
+        '''
+        # Put your execute step code here before calling the '_doneExecution' method.
+        self._data.load(self._config['Location'])
+        self._doneExecution()
+
+    def getPortData(self, index):
+        '''
+        Add your code here that will return the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        provides port for this step then the index can be ignored.
+        '''
+        # http://physiomeproject.org/workflow/1.0/rdf-schema#trcdata
+        return self._data
+
+    def configure(self):
+        '''
+        This function will be called when the configure icon on the step is
+        clicked.  It is appropriate to display a configuration dialog at this
+        time.  If the conditions for the configuration of this step are complete
+        then set:
+            self._configured = True
+        '''
+        dlg = ConfigureDialog()
+        dlg.identifierOccursCount = self._identifierOccursCount
+        dlg.setConfig(self._config)
+        dlg.validate()
+        dlg.setModal(True)
+
+        if dlg.exec_():
+            self._config = dlg.getConfig()
+
+        self._configured = dlg.validate()
+        self._configuredObserver()
+
+    def getIdentifier(self):
+        '''
+        The identifier is a string that must be unique within a workflow.
+        '''
+        return self._config['identifier']
+
+    def setIdentifier(self, identifier):
+        '''
+        The framework will set the identifier for this step when it is loaded.
+        '''
+        self._config['identifier'] = identifier
+
+    def serialize(self, location):
+        '''
+        Add code to serialize this step to disk.  The filename should
+        use the step identifier (received from getIdentifier()) to keep it
+        unique within the workflow.  The suggested name for the file on
+        disk is:
+            filename = getIdentifier() + '.conf'
+        '''
+        configuration_file = os.path.join(location, self.getIdentifier() + '.conf')
+        conf = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
+        conf.beginGroup('config')
+        conf.setValue('identifier', self._config['identifier'])
+        conf.setValue('Location', self._config['Location'])
+        conf.endGroup()
+
+
+    def deserialize(self, location):
+        '''
+        Add code to deserialize this step from disk.  As with the serialize 
+        method the filename should use the step identifier.  Obviously the 
+        filename used here should be the same as the one used by the
+        serialize method.
+        '''
+        configuration_file = os.path.join(location, self.getIdentifier() + '.conf')
+        conf = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
+        conf.beginGroup('config')
+        self._config['identifier'] = conf.value('identifier', '')
+        self._config['Location'] = conf.value('Location', '')
+        conf.endGroup()
+
+        d = ConfigureDialog()
+        d.identifierOccursCount = self._identifierOccursCount
+        d.setConfig(self._config)
+        self._configured = d.validate()
+
+
