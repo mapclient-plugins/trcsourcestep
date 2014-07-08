345c2575f82f6b959bb14b5a95f7dd1cb2cad1cf
juzhang <jzha263@aucklanduni.ac.nz>
2014-07-08 08:28:22 -0400

    added new icon

--- mapclientplugins/trcsourcestep/__init__.py
+++ mapclientplugins/trcsourcestep/__init__.py
@@ -8,4 +8,4 @@ __author__ = 'Hugh Sorby'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.trcsourcestep import step
-
+import mapclientplugins.trcsourcestep.resources_rc
