679c4bb18d8e486192480f75f6622b62f058c6ce
juzhang <jzha263@aucklanduni.ac.nz>
2015-01-15 11:50:52 +1300

    updated to be python3 compliant, __stepname__ and __location__ added

--- mapclientplugins/trcsourcestep/__init__.py
+++ mapclientplugins/trcsourcestep/__init__.py
@@ -4,6 +4,9 @@ MAP Client Plugin
 '''
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
+__stepname__ = 'TRC Source'
+__location__ = 'https://github.com/mapclient-plugins/trcsourcestep/archive/master.zip'
+
 
 
 # import class that derives itself from the step mountpoint.
