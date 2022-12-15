70f2465091c76794898f7e418a336b0946ab7654
TimmyNic <tim.nicolas@outlook.com>
2022-12-15 15:40:37 +1300

    Set version to 0.2.0

--- mapclientplugins/trcsourcestep/__init__.py
+++ mapclientplugins/trcsourcestep/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'TRC Source'
 __location__ = 'https://github.com/mapclient-plugins/trcsourcestep/archive/v0.1.1.zip'
